ee98805cc167eccd003b09cc5a0d862cbdd0d453
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-05-12 10:15:13 +0200

    creation_struct_FBM

--- src/noise.hpp
+++ src/noise.hpp
@@ -7,3 +7,10 @@ float perlinNoise(glm::vec2 const& position);
 float perlinNoiseSeeded(glm::vec2 const& position, int seed);
 
 float octaveNoise(glm::vec2 const& position, std::function<float(glm::vec2 const&)> noiseFunction);
+
+struct FBM {
+    int octaves = 2;
+    float lacunarity = 0.5f;
+    float gain = 0.5f;
+    float scale = 4.0f; 
+};
