f24d377f6455bbb1cef33bc8e2bdbf57bead72e5
Alexandre Bonnet <alexandre.bonnet@edu.esiee.fr>
2026-06-07 18:38:19 +0200

    Added all 3d models

--- src/draw.hpp
+++ src/draw.hpp
@@ -6,5 +6,6 @@
 void draw3DScene(AppContext& context);
 void drawRaylibUI(AppContext& context);
 void drawCubes(AppContext const& context, Matrix const& terrainCentering);
+void drawBoat(AppContext const& context, Matrix const& terrainCentering);
 
 void drawImGui(AppContext& context);
